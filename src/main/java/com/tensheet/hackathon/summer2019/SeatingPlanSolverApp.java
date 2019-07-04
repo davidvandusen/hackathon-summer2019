9a898ff06ae0ba7a8631afb3ee42cde0f3bc8d12
David VanDusen <david.vandusen@bench.co>
2019-07-04 12:46:41 -0700

    Reduce run time; add event listener for new best solution

--- src/main/java/com/tensheet/hackathon/summer2019/SeatingPlanSolverApp.java
+++ src/main/java/com/tensheet/hackathon/summer2019/SeatingPlanSolverApp.java
@@ -2,10 +2,20 @@ package com.tensheet.hackathon.summer2019;
 
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
+import org.optaplanner.core.api.solver.event.BestSolutionChangedEvent;
+import org.optaplanner.core.api.solver.event.SolverEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SeatingPlanSolverApp {
+
+    private static class NewBestScoreListener implements SolverEventListener<SeatingPlanSolution> {
+        @Override
+        public void bestSolutionChanged(BestSolutionChangedEvent<SeatingPlanSolution> event) {
+            SeatingPlanSolution newBestSolution = event.getNewBestSolution();
+            System.out.println("New best score: " + newBestSolution.getScore());
+        }
+    }
 
     private static final Logger LOG = LoggerFactory.getLogger(SeatingPlanSolverApp.class);
 
@@ -16,6 +26,8 @@ public class SeatingPlanSolverApp {
         SeatingPlanSolution planningProblem = SeatingPlanSolutionFactory.createFromYamlResource(args[0]);
         LOG.info("Building solver from file (" + args[1] + ").");
         Solver solver = SolverFactory.createFromXmlResource(args[1]).buildSolver();
+        NewBestScoreListener newBestScoreListener = new NewBestScoreListener();
+        solver.addEventListener(newBestScoreListener);
         solver.solve(planningProblem);
         SeatingPlanSolution solution = (SeatingPlanSolution) solver.getBestSolution();
         SeatingPlanSolutionPresenter presenter = new SeatingPlanSolutionPresenter(solution);
